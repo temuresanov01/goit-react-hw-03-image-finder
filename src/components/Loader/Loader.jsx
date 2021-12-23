1eea8e0ab023078567edd1c9dbc62242f4ecb7b9
Temur Esanov <temuresanov01@gmail.com>
2021-12-23 10:17:11 +0100

    react-starter v3

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+// import Loader from "react-loader-spinner";
+
+// const Spinner = () => {
+//   return (
+//     <>
+//       <Loader
+//         type="ThreeDots"
+//         color="#00000"
+//         height={80}
+//         width={80}
+//         timeout={2300}
+//       />
+//     </>
+//   );
+// };
+// export default Spinner;
